d4eef34df2f8c14e6e5324138c4898021c65d3b0
Moacir Amaro <moacir@gmail.com>
2021-09-27 20:47:28 -0300

    check leak

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 14:09:52 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/27 20:44:23 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/27 20:47:19 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 char	*get_next_line(int fd)
 {
-	char			*buffer;
+	char			buffer[BUFFER_SIZE + 1];
 	size_t			size_read;
 	char			*line;
 
 	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, buffer, 0) < 0)
 		return (NULL);
-	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	size_read = read(fd, buffer, BUFFER_SIZE);
 	if (size_read < 0 )
 		return (NULL);
 	line = read_file(size_read, buffer, fd);
-	free(buffer);
 	return (line);
 }
 
