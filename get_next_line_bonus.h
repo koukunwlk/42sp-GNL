57bb2424793e2b23bc56674f90080c65ea11b403
Moacir Amaro <moacir@gmail.com>
2021-10-04 17:46:56 -0300

    Norm fix

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 14:09:55 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/27 21:22:44 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/10/04 17:46:47 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
+
 char	*get_next_line(int fd);
 char	*read_file(size_t size_read, char *buffer, int fd);
 char	*ft_strjoin_and_free(char *s1, char *s2);
@@ -24,4 +25,5 @@ char	*ft_strdup(char *s);
 char	*ft_strchr(char *str, char c);
 size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+
 #endif
